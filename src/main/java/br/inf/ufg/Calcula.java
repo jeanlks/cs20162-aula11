17a8ee8ec2f56d9f89d37f8a6daa9941f6923450
Jean Lucas Monte Carvalho <jean.rv@hotmail.com>
2016-11-02 16:23:45 -0200

    Funcionando, precisa refatoração

--- src/main/java/br/inf/ufg/Calcula.java
+++ src/main/java/br/inf/ufg/Calcula.java
@@ -1,10 +1,38 @@
 package br.inf.ufg;
 
+import java.util.List;
 
+import com.github.kyriosdata.parser.*;
 public class Calcula {
-	public static void main(String []args){
-		if(args.length>0){
-		String expr = args[0];
-		System.out.println(expr);
+    public float calculaValor(String expr){
+	    Expressao expressao = exprPara(expr);
+		return expressao.valor();
+	}
+	private Expressao exprPara(String expressao) {
+        List<Token> tokens = new Lexer(expressao).tokenize();
+        Parser parser = new Parser(tokens);
+        return parser.expressao();
+	}
+
+    public void executa(String expr){
+	  if(expr ==null){
+		    System.out.println("Informe a expressão");
+	  }
+	  try{
+	      float valor = calculaValor(expr);
+	      System.out.println(valor);
+	      System.exit(0);
+	  }catch (IllegalArgumentException ex){
+	      System.out.print(ex.getMessage());
+	      System.exit(1);
+	  }
+    }
+    public static void main(String []args){
+       if(args.length>0){
+       String expr = args[0];
+       Calcula calcula = new Calcula();
+       calcula.executa(expr);
+       
 	}}
+	
 }
