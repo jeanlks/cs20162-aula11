165aa1b8b90b3e765cf05b6e1bf72b6128cfb3a5
Jean Lucas Monte Carvalho <jean.rv@hotmail.com>
2016-11-02 17:14:52 -0200

    Refatoração feita

--- src/main/java/br/inf/ufg/Calcula.java
+++ src/main/java/br/inf/ufg/Calcula.java
@@ -1,38 +1,69 @@
+/*
+ * Copyright (c) 2016. Jean Lucas Monte Carvalho
+ * Creative Commons Attribution 4.0 International License.
+ */
 package br.inf.ufg;
 
 import java.util.List;
 
-import com.github.kyriosdata.parser.*;
+import com.github.kyriosdata.parser.Expressao;
+import com.github.kyriosdata.parser.Parser;
+import com.github.kyriosdata.parser.Token;
+import com.github.kyriosdata.parser.Lexer;
+
+/**
+ * Implementação do algoritmo Calcular.
+ *
+ * Algoritmo implementado para aula de Construção
+ * de software e se utiliza da biblioteca parser.
+ *
+ */
 public class Calcula {
-    public float calculaValor(String expr){
-	    Expressao expressao = exprPara(expr);
-		return expressao.valor();
-	}
-	private Expressao exprPara(String expressao) {
+    /**
+     * @param expr expressao de calculo
+     * @return valor da expressao
+     */
+     public final float calculaValor(final String expr) {
+        Expressao expressao = exprPara(expr);
+        return expressao.valor();
+     }
+    /**
+     * @param expressao expressao para calculo
+     * @return expressao
+     */
+     private Expressao exprPara(final String expressao) {
         List<Token> tokens = new Lexer(expressao).tokenize();
         Parser parser = new Parser(tokens);
         return parser.expressao();
-	}
+     }
 
-    public void executa(String expr){
-	  if(expr ==null){
-		    System.out.println("Informe a expressão");
-	  }
-	  try{
-	      float valor = calculaValor(expr);
-	      System.out.println(valor);
-	      System.exit(0);
-	  }catch (IllegalArgumentException ex){
-	      System.out.print(ex.getMessage());
-	      System.exit(1);
-	  }
+     /**
+     * @param expr
+     * expressao a ser executada
+     */
+    public final void executa(final String expr) {
+       if (expr == null) {
+           System.out.println("Informe a expressão");
+       }
+       try {
+           float valor = calculaValor(expr);
+           System.out.println(valor);
+           System.exit(0);
+       } catch (IllegalArgumentException ex) {
+           System.out.print(ex.getMessage());
+           System.exit(1);
+       }
     }
-    public static void main(String []args){
-       if(args.length>0){
+    /**
+     *
+     * @param args
+     * parametros usados para calculo
+     */
+    public static void main(final String[]args) {
+       if (args.length > 0) {
        String expr = args[0];
        Calcula calcula = new Calcula();
        calcula.executa(expr);
-       
-	}}
-	
+     }
+   }
 }
---
+++ src/test/java/AlgoritmosTest.java
@@ -0,0 +1,9 @@
+import static org.junit.Assert.*;
+import br.inf.ufg.*;
+import org.junit.Test;
+
+public class AlgoritmosTest {
+
+
+    
+}
